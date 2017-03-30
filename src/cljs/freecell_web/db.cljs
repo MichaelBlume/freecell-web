7458ef57aa0ad3f6fee28dd91307cc9a6da9bb10
Michael Blume <blume.mike@gmail.com>
2017-03-29 18:41:19 -0700

    states

--- src/cljs/freecell_web/db.cljs
+++ src/cljs/freecell_web/db.cljs
@@ -1,4 +1,33 @@
-(ns freecell-web.db)
+(ns freecell-web.db
+  (:require [freecell-web.cards :refer [shuffled-deck make-columns]]))
 
 (def default-db
   {:name "re-frame"})
+
+(defn init-ui []
+  {:selected-column nil})
+
+(defn init-state
+  ([] (init-state (shuffled-deck)))
+  ([deck])
+    {:undo-states nil
+     :redo-states nil
+     :ui-state (init-ui)
+     :cards-state
+     {:columns (make-columns deck)
+      :freecells (into [] (repeat 4 nil))
+      :sinks {:spades 0 :clubs 0 :diamonds 0 :hearts 0}}})
+
+(defn undo [{:keys [undo-states redo-states cards-state] :as state}]
+  (when (seq undo-states)
+    {:undo-states (rest undo-states)
+     :redo-states (cons cards-state redo-states)
+     :ui-state (init-ui)
+     :cards-state (first undo-states)}))
+
+(defn redo [{:keys [undo-states redo-states cards-state] :as state}]
+  (when (seq redo-states)
+    {:undo-states (cons cards-state undo-states)
+     :redo-states (rest undo-states)
+     :ui-state (init-ui)
+     :cards-state (first redo-states)}))
