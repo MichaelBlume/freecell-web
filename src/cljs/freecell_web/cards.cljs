d783373245058d55ac9b97a35dd28f1519e9697c
Michael Blume <blume.mike@gmail.com>
2017-03-29 18:39:58 -0700

    set up column state

--- src/cljs/freecell_web/cards.cljs
+++ src/cljs/freecell_web/cards.cljs
@@ -29,3 +29,15 @@
 
 (defn display-string [card]
   (str (face-chars (:n card) (:n card)) (suit-chars (:suit card))))
+
+(defn flip [vs] (apply map vector vs))
+
+(defn make-columns [deck]
+  (->>
+    deck
+    (concat (repeat 4 nil))
+    (partition 8)
+    flip
+    (map #(filter identity %))
+    reverse
+    (into [])))
