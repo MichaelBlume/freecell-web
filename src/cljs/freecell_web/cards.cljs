22bb8f1195f3b9435bffab77952525ea3cfad93b
Michael Blume <blume.mike@gmail.com>
2017-03-29 18:38:36 -0700

    display strings

--- src/cljs/freecell_web/cards.cljs
+++ src/cljs/freecell_web/cards.cljs
@@ -15,3 +15,17 @@
       {:n n :suit suit})))
 
 (defn shuffled-deck [] (shuffle deck))
+
+(def suit-chars
+  {:spades \S
+   :hearts \H
+   :clubs \C
+   :diamonds \D})
+
+(def face-chars
+  {11 \J
+   12 \Q
+   13 \K})
+
+(defn display-string [card]
+  (str (face-chars (:n card) (:n card)) (suit-chars (:suit card))))
