c605870b1e468cfdd2b83c3292f37c67e4fbe605
Michael Blume <blume.mike@gmail.com>
2017-03-29 18:40:26 -0700

    some card actions

--- src/cljs/freecell_web/cards.cljs
+++ src/cljs/freecell_web/cards.cljs
@@ -41,3 +41,65 @@
     (map #(filter identity %))
     reverse
     (into [])))
+
+(defn goes-on [card on]
+  (or
+    (nil? on)
+    (and
+      (not (= (color card) (color on)))
+      (= (:n on) (inc (:n card))))))
+
+(defn moveable-subset [column]
+  (when (seq column)
+    (cons
+      (first column)
+      (when (goes-on (first column) (second column))
+        (moveable-subset (rest column))))))
+
+(defn move-column [from to moveable]
+  (let [moveable-cards (take moveable (moveable-subset from))]
+    (some identity
+      (for [i (range (count moveable-cards) 0 -1)]
+        (let [to-move (take i moveable-cards)]
+          (when (goes-on (last to-move) (first to))
+            [(drop i from) (concat to-move to) i]))))))
+
+(defn run-move [columns from to moveable]
+  (let [from-col (nth columns from)
+        to-col (nth columns to)]
+    (when-let [[new-from new-to] (move-column from-col to-col moveable)]
+      (-> columns
+          (assoc from new-from)
+          (assoc to new-to)))))
+
+(defn can-sink [card sinks]
+  (= (:n card) (inc (sinks (:suit card)))))
+
+(defn sink [card sinks]
+  (assoc sinks (:suit card) (:n card)))
+
+(defn opposite-color [card]
+  (if (= :black (color card))
+    :red
+    :black))
+
+(defn min-sink [sinks]
+  (apply min (vals sinks)))
+
+(defn min-opposite-sink [card sinks]
+  (apply
+    min
+    (for [[suit n] sinks
+          :when (= (colors suit) (opposite-color card))]
+      n)))
+
+(defn should-sink [card sinks]
+  (and
+    (can-sink card sinks)
+    (or
+      ; Below cards are both already up
+      (<= (:n card) (inc (min-opposite-sink card sinks)))
+      ; If we have both black aces we can put up a red 3
+      (and
+        (<= (- (:n card) (min-sink sinks)) 3)
+        (<= (- (:n card) (min-opposite-sink card sinks)) 2)))))
